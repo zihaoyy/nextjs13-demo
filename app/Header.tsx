ed5c466201b780b360b9f6a46508232a3d5b5865
Zihao <zihao@adoi.cn>
2022-12-13 11:18:09 +0800

    feat: add serverside todoslist page

--- app/Header.tsx
+++ app/Header.tsx
@@ -1,9 +1,10 @@
+import Link from 'next/link'
 import React from 'react'
 
 function Header() {
   return (
     <header className='p-5 bg-blue-500'>
-      <p className='font-bold text-white'>I am the Header</p>
+      <Link href='/' className='px-2 py-1 bg-white text-blue-500 rounded-lg'>Home</Link>
     </header>
   )
 }
