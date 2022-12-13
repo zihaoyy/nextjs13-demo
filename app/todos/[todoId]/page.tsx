11a9af5e686937909423fb54735d8421ed4591ee
Zihao <zihao@adoi.cn>
2022-12-13 11:43:06 +0800

    refactor: use ssg to pre render the first ten pages of todo

--- app/todos/[todoId]/page.tsx
+++ app/todos/[todoId]/page.tsx
@@ -8,7 +8,10 @@ type PageProps = {
 }
 
 const fetchTodo = async (todoId: string) => {
-  const response = await fetch(`https://jsonplaceholder.typicode.com/todos/${todoId}`)
+  const response = await fetch(`https://jsonplaceholder.typicode.com/todos/${todoId}`, {
+    // server-side caching: 'no-cache', client-side caching: 'force-cache', ssg caching: next: { revalidate: 60 }
+    next: { revalidate: 60 }
+  })
   const todo: Todo = await response.json()
   return todo
 }
@@ -31,3 +34,11 @@ async function TodoPage({ params: { todoId } }: PageProps) {
 }
 
 export default TodoPage
+
+export async function generateStaticParams() {
+  const response = await fetch('https://jsonplaceholder.typicode.com/todos/')
+  const todos: Todo[] = await response.json()
+  // for this DEMO, we are only prebuilding the first 10 pages to avoid being rate limited by the API
+  const trimedTodos = todos.slice(0, 10)
+  return trimedTodos.map((todo) => ({ todoId: todo.id.toString() }))
+}
